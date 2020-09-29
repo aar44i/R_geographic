1b2b9a0bdb72884ce44f32538ef1a390f7d9feee
Artem <aar44i@yandex.ru>
2020-09-29 13:46:26 +0300

    1

--- R/HW/Gorbarenko_4/4_Gorbarenko.R
+++ R/HW/Gorbarenko_4/4_Gorbarenko.R
@@ -2,23 +2,23 @@ library(readxl)
 library(tidyverse)
 library(writexl)
 
-soil_data = read_excel('soil_data.xls', 1,  # С‡С‚РµРЅРёРµ С„Р°РёР»Р° 
+soil_data = read_excel('soil_data.xls', 1,  # чтение фаила 
                 col_types = c(rep('numeric', 2), rep('text', 240)))
 description = read_excel('soil_data.xls',2 , col_names = FALSE, 
                 col_types = c(rep('text', 2)))
 colnames(description) = c('name', 'desc')
 
-check_completeness = function(df){ # РЅР°РїРёСЃР°РЅРёРµ С„СѓРЅРєС†РёРё РґР»СЏ РіРµРЅРµСЂР°С†РёРё РЅРѕРІРѕРіРѕ С„СЂРµР№РјР° СЃРѕ СЃС‚РѕР»Р±С†Р°РјРё name, completeness, status
+check_completeness = function(df){ # написание функции для генерации нового фрейма со столбцами name, completeness, status
   name = colnames(df)
   completeness = sapply(df, function(X) {round((length(X) - sum(is.na(X))) / length(X) * 100, digits = 0)})
-  status = sapply(completeness, function(X) {if (X == 100) status = 'РїРѕР»РЅРѕСЃС‚СЊСЋ' 
-                                              else if (X == 0) status = 'РЅРµ Р·Р°РїРѕР»РЅРµРЅ' 
-                                              else status = 'С‡Р°СЃС‚РёС‡РЅРѕ'})
+  status = sapply(completeness, function(X) {if (X == 100) status = 'полностью' 
+                                              else if (X == 0) status = 'не заполнен' 
+                                              else status = 'частично'})
   df2 = data.frame(name, completeness, status)
 }
 
-soil_types = split(soil_data, soil_data$SOIL_ID) # СЂР°Р·РґРµР»РµРЅРёРµ С„СЂРµР№РјР° РЅР° С„СЂРµР№РјС‹ РїРѕ РєР°Р¶РґРѕРјСѓ С‚РёРїСѓ РїРѕС‡РІ
-soil_types2 = lapply(soil_types, check_completeness) # РїСЂРёРјРµРЅРµРЅРёРµ С„СѓРЅРєС†РёРё copleteness РєРѕ РІСЃРµРј С„СЂРµР№РјР°Рј РґР°РЅРЅС‹С…
-complete = lapply(soil_types2, function(X) {transmute(X, name = name, desc = description$desc, # РґРѕР±Р°РІР»РµРЅРёРµ СЂР°СЃСЃС€РёС„СЂРѕРІРєРё РґР»СЏ РєР°Р¶РґРѕРіРѕ SOIL_ID
+soil_types = split(soil_data, soil_data$SOIL_ID) # разделение фрейма на фреймы по каждому типу почв
+soil_types2 = lapply(soil_types, check_completeness) # применение функции copleteness ко всем фреймам данных
+complete = lapply(soil_types2, function(X) {transmute(X, name = name, desc = description$desc, # добавление рассшифровки для каждого SOIL_ID
                                                       completeness = completeness, status = status)})
-write_xlsx(complete, 'completeness.xlsx') # РІС‹РІРѕРґ РіРѕС‚РѕРІРѕРіРѕ С„Р°РёР»Р° 
+write_xlsx(complete, 'completeness.xlsx') # вывод готового фаила 

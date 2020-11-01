4dee07d9d26a2611141fe1f4ea32415f9d954f12
Artem <aar44i@yandex.ru>
2020-11-01 21:50:22 +0300

    1

--- R_HydroPredictions/Class/Mine/6_P.R
+++ R_HydroPredictions/Class/Mine/6_P.R
@@ -1,46 +1,44 @@
 library(readxl)
-
+library(ggplot2)
 setwd('C:/Users/gorba/DataSciense/R_geographic/R_HydroPredictions/Class/Mine')
-# C:\Users\gorba\DataSciense\R_geographic\R_HydroPredictions\Class\Mine
-# load('baikal.xlsx')
 baikal = read_excel('baikal.xlsx')
 baikal = na.omit(baikal) #
 
-baikal_apr = baikal[baikal$month == 'Apr',]
+baikal_aug = baikal[baikal$month == 'Aug',]
 
 # август
-ggplot(baikal_apr, aes(x = year)) + geom_point(aes(y = pred, col = 'прогноз')) + 
+ggplot(baikal_aug, aes(x = year)) + geom_point(aes(y = pred, col = 'прогноз')) + 
   geom_line(aes(y = pred, col= 'Прогноз')) + 
   geom_point(aes(y = obs, col = 'Наблюдения')) + 
   geom_line(aes(y = obs, col = 'Наблюдения'))
 
-mean_pred = mean(baikal_apr$pred)
-sd_pred = sd(baikal_apr$pred)
-sd_fact = sd(baikal_apr$obs)
+mean_pred = mean(baikal_aug$pred)
+sd_pred = sd(baikal_aug$pred)
+sd_fact = sd(baikal_aug$obs)
 
-baikal_apr$err = baikal_apr$pred - baikal_apr$obs
-baikal_apr$AE = abs(baikal_apr$pred - baikal_apr$obs)
+baikal_aug$err = baikal_aug$pred - baikal_aug$obs
+baikal_aug$AE = abs(baikal_aug$pred - baikal_aug$obs)
 
-ME = mean(baikal_apr$err)
-MAE = mean(baikal_apr$AE)
+ME = mean(baikal_aug$err)
+MAE = mean(baikal_aug$AE)
 
-MSE = mean(baikal_apr$err ^ 2)
+MSE = mean(baikal_aug$err ^ 2)
 RMSE = sqrt(MSE)
 
 SSc = RMSE / sd_fact
 
-R = cor(baikal_apr$pred, baikal_apr$obs)
+R = cor(baikal_aug$pred, baikal_aug$obs)
 
 
-ggplot(baikal_apr, aes(x = obs, y = pred, col = err)) + 
-  geom_point(size = 2) + xlim(0,2000) + ylim(0,2000)+ geom_abline() + 
+ggplot(baikal_aug, aes(x = obs, y = pred, col = err)) + 
+  geom_point(size = 3) + xlim(0,2000) + ylim(0,2000) + geom_abline() + 
   geom_vline(xintercept = 1000) + geom_hline(yintercept  = 1000)
 
-baikal_apr$opr = baikal_apr$err >=0.674 * sd_fact
-summary(baikal_apr)
+baikal_aug$opr = baikal_aug$err >=0.674 * sd_fact
+summary(baikal_aug)
 
 
-OPR = sum(baikal_apr$opr) / length(baikal_apr$opr)
+OPR = sum(baikal_aug$opr) / length(baikal_aug$opr)
 
 error_check = function(x){
   mean_pred = mean(x$pred)
@@ -54,11 +52,11 @@ error_check = function(x){
   MSE = mean(x$err ^ 2)
   RMSE = sqrt(MSE)
   SSc = RMSE / sd_fact
-  R = cor(baikal_apr$pred, baikal_apr$obs)
+  R = cor(baikal_aug$pred, baikal_aug$obs)
   result = list(mean_pred, mean_fact, sd_pred, sd_fact, ME, MAE, MSE, RMSE, SSc, R)
   # names(result) = c('F_mean')
   return(result)
 }
 
-error_apr = error_check(baikal_apr)
+error_apr = error_check(baikal_aug)
 error_apr
